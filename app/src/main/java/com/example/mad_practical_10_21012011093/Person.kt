40cd774354125a74eb1700b329a410570643837c
Nandinee <nandineepatel2003@gmail.com>
2023-10-27 15:06:00 +0530

    Practical-10

--- app/src/main/java/com/example/mad_practical_10_21012011093/Person.kt
+++ app/src/main/java/com/example/mad_practical_10_21012011093/Person.kt
@@ -1,6 +1,7 @@
 package com.example.mad_practical_10_21012011093
 
 import android.security.identity.AccessControlProfileId
+import org.json.JSONObject
 import java.io.Serializable
 import java.net.Inet4Address
 
@@ -11,4 +12,16 @@ class Person(var id: String,
              var address: String,
              var latitude: Double,
              var longitude: Double):Serializable {
+
+    constructor(jsonObject: JSONObject):this("","","","","",0.0,0.0) {
+        id = jsonObject.getString("id")
+        emailid = jsonObject.getString("email")
+        phoneNo = jsonObject.getString("phone")
+        val profileJson = jsonObject.getJSONObject("profile")
+        name = profileJson.getString("name")
+        address = profileJson.getString("address")
+        val locationJson = profileJson.getJSONObject("location")
+        latitude = locationJson.getDouble("lat")
+        longitude = locationJson.getDouble("long")
+    }
 }
